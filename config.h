267fdf7ed36c6483a081195a8c663f9d0c37e93e
Bartosz Piech <piech.bartosz696@gmail.com>
2022-09-14 12:46:54 +0200

    fixed some changes'

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const Gap default_gap        = {.isgap = 1, .realgap = 20, .gappx = 5};
+static const Gap default_gap        = {.isgap = 1, .realgap = 20, .gappx = 20};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -34,7 +34,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "qutebrowser",  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "qutebrowser",  NULL,   NULL,       1 << 1,       0,           -1 },
     { "tabbed",   NULL,       NULL,       0,            1,           -1 },
 };
 
